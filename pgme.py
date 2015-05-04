348dfdd0d2521f64d23217d98f57ba4c31d3794f
JenniferlHood <jenniferl.hood@carleton.ca>
2015-05-04 13:50:18 -0500

    add vertices and edges with mouse click

--- pgme.py
+++ pgme.py
@@ -1,8 +1,8 @@
 from __future__ import division
-import sys
+#import sys
 import math 
 import pygame
-import random
+
 
 
 # colour globals
@@ -25,12 +25,8 @@ class pgmeMain(object):
 		self.REFRESH = pygame.USEREVENT+1
 		pygame.time.set_timer(self.REFRESH, 1000//self.FPS)
 
-		#list of vertices for drawing on the screen
-		self.listOfVertices = []
-
-		#adjacency list for graph representation
-		self.adjacent = []
-
+		
+		#program variables
 		#vertex list 
 		self.vList = [(100,100),(300,500),(500,300)]
 		
@@ -40,7 +36,9 @@ class pgmeMain(object):
 
 		self.aList = [[self.vList[1],self.vList[2]], \
 			[self.vList[0],self.vList[2]],[self.vList[0],self.vList[1]]]
-
+		
+		# selected vertex (a tuple of indices)
+		verSelect = (None,None)
 
 		#after initialization, run the main program loop
 		self.event_loop()
@@ -49,9 +47,26 @@ class pgmeMain(object):
 
 		while True:
 			event = pygame.event.wait()
+		
 			if event.type == pygame.QUIT:
 				sys.exit()
-			if event.type == self.REFRESH:
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				#for a left click, add a vertex at the clicked coordinate				
+				if pygame.mouse.get_pressed() == (1,0,0):
+					self.vList.append(pygame.mouse.get_pos())
+				
+				#select two vertices to connect with an edge
+				elif pygame.mouse.get_pressed() == (0,0,1):
+					pos = pygame.mouse.get_pos()
+					
+					for i in self.vList:
+						if (i[0] - 10) < pos[0] < (i[0] + 10) and \
+							(i[1] - 10) < pos[1] < (i[1] + 10):
+
+							self.verSelect[0] == self.vlist.index(i)
+
+
+			elif event.type == self.REFRESH:
 				self.draw()
 			else:
 				pass
@@ -65,14 +80,27 @@ class pgmeMain(object):
 
 		
 		# Draw the edges of adjacent vertecies:
-		# Draw a line from the ith vertex in the list
-		# to each of the vertexes listed in the corresponding
+		# Draw a line from the i,jth vertex in the alist 
+		# to each of the vertexes listed in the corresponding ith vertex in the vlist
 		vIndexCount = 0
 		for i in self.aList:
 			for j in i:
 				pygame.draw.line(self.screen,LAV,self.vList[vIndexCount],j, 2)
 			vIndexCount += 1
-		
+
+
+			
+		#highlight the first selected vertex and draw the prospective edge
+		if self.verSelect[0] is not None:
+			vertex = self.verSelect[0]
+			edge = pygame.mouse.get_pos()
+			
+			#redraw the selected vertex in a distinct colour
+			pygame.draw.circle(self.screen,PEA,self.vList(vertex)
+			
+			#draw the prospective edge in a distinct colour
+			pygame.draw.line(self.screen,CORAL,self.vList(vertex),edge,2)
+
 
 		pygame.display.flip()
 pgmeMain()
